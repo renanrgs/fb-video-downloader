0904ee2900bb448abb64176228342bb2df47ea4d
Renan Guedes <renan.rgsilva@gmail.com>
2022-05-30 08:28:21 +0200

    Improving import statements

--- src/facebook_video_downloader/model/video.py
+++ src/facebook_video_downloader/model/video.py
@@ -2,8 +2,7 @@ from dataclasses import dataclass, field
 
 import requests
 from facebook_video_downloader.config.config import cookies, headers
-from facebook_video_downloader.regex.fb_downloader_regex import (
-    get_audio_link, get_video_data, get_video_id, get_video_links)
+from facebook_video_downloader.regex import fb_downloader_regex
 
 
 @dataclass
@@ -15,8 +14,8 @@ class Video:
     resolutions_links: dict[str, str] = field(init=False, default_factory=dict[str, str])
 
     def __post_init__(self):
-        self.id = get_video_id(self.url)
+        self.id = fb_downloader_regex.get_video_id(self.url)
         page_content = requests.get(self.url, cookies=cookies(), headers=headers()).text
-        self.html_content = get_video_data(page_content, self.id)
-        self.audio_link = get_audio_link(self.html_content)
-        self.resolutions_links = get_video_links(self.html_content)
+        self.html_content = fb_downloader_regex.get_video_data(page_content, self.id)
+        self.audio_link = fb_downloader_regex.get_audio_link(self.html_content)
+        self.resolutions_links = fb_downloader_regex.get_video_links(self.html_content)
